222ad6cc54c3f142ea733bcf22b27ebc2ccc1760
sbensadoun <samson.bensadoun@technosens.fr>
2023-12-28 06:56:41 +0100

    crud et bdd mango

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const Thing = require('./models/Thing');
+const Product = require('./models/Product');
 const app = express();
 
+mongoose.connect('mongodb+srv://sbensadoun:microice06@node.kxzijqc.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -9,26 +19,74 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
+// Product part 
+
+app.post('/api/products', (req, res, next) => {
+  const product = new Product({
+    ...req.body
   });
+  product.save()
+    .then(product => res.status(201).json(product))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/products/:id', (req, res, next) => {
+  Product.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({message: 'Deleted!'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.put('/api/products/:id', (req, res, next) => {
+  Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({message: 'Modified!'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/products/:id', (req, res, next) => {
+  Product.findOne({ _id: req.params.id })
+    .then(product => res.status(200).json(product))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.get('/api/products', (req, res, next) => {
+  Product.find()
+    .then(products => res.status(200).json(products))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.post('/api/stuff', (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.get('/api/stuff', (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+});
+
 
 module.exports = app;
