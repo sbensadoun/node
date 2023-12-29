d6d0e86c5372e2eff197b8407c81851db4d6fc4e
sbensadoun <samson.bensadoun@technosens.fr>
2023-12-29 08:53:59 +0100

    gestion utilisateur , auth , hash mdp , token

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://sbensadoun:microice06@node.kxzijqc.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -19,5 +21,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 module.exports = app;
